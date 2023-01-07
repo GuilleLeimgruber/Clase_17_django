341912cf4d8863d02a86886946feab39f0b5d909
Guillermo Leimgruber <leimgruberguille@gmail.com>
2023-01-07 07:51:24 -0300

    Este commit tiene hasta el template de la clase XVII

--- python_34650/python_34650/urls.py
+++ python_34650/python_34650/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from python_34650.views import hola_mundo, otra_vista_mas
+from python_34650.views import hola_mundo, otra_vista_mas, fecha_de_hoy, vista_con_edad,mi_nombres_es, vista_con_template
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hola/', hola_mundo),
     path('otra_vista/', otra_vista_mas),
+    path('fecha/', fecha_de_hoy),
+    path('edad/<int:edad>/', vista_con_edad),
+    path('nombre/<str:nombre>', mi_nombres_es),
+    path('vista_template/', vista_con_template)
 ]
--- python_34650/python_34650/views.py
+++ python_34650/python_34650/views.py
@@ -1,5 +1,7 @@
 
 from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
 
 
 
@@ -9,3 +11,17 @@ def hola_mundo(request):
 
 def otra_vista_mas(request):
     return HttpResponse("Aca hay otra vista mas!!!") 
+
+def fecha_de_hoy(request):
+    hoy = datetime.now().date()
+    return HttpResponse(f'La fecha de hoy es {hoy}')
+
+def vista_con_edad(request, edad):
+    return HttpResponse(f'Mi edad es {edad}')
+
+def mi_nombres_es(self, nombre):
+    documento_texto = f"Mi nonbre es: <br> <br> {nombre}"    
+    return HttpResponse(documento_texto)
+
+def vista_con_template(request):
+    return render(request, 'template_1.html', context = {})
